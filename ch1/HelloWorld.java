029eea380838cbacecb0eebd2b4dcdee5226bb92
youngkiSeo <130648198+youngkiSeo@users.noreply.github.com>
2023-04-18 17:54:57 +0900

    Add files via upload

---
+++ ch1/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.green.java.ch01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ch1/Main.java
@@ -0,0 +1,12 @@
+package com.green.java.ch01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        System.out.println(a.split(" "));
+
+    }
+}
